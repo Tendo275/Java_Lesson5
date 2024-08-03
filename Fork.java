1e9c73aaae6a2a71b13898ee756d3e54c30b32df
Tendo275 <kulikov.sy@mail.ru>
2024-08-03 11:12:22 +0300

    new commit

---
+++ Fork.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.seminar_5;
+
+/**
+ * Fork - класс "Вилки".
+ */
+public class Fork {
+    public int number;         // Вилка имеет порядковый номер.
+
+    public Fork(int number) {
+        this.number = number;
+    }
+}
